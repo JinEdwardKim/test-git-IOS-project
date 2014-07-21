2a8e280342b6e035d0a0421ee6c6373f7e3c8584
Edward <edward@Jins-MacBook-Pro.local>
2014-07-21 10:04:45 +1000

    added a test class

---
+++ JIN_Test.h
@@ -0,0 +1,13 @@
+//
+//  JIN_Test.h
+//  Git Test
+//
+//  Created by Edward on 21/07/2014.
+//  Copyright (c) 2014 Jin Edward Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JIN_Test : NSObject
+
+@end
---
+++ JIN_Test.m
@@ -0,0 +1,13 @@
+//
+//  JIN_Test.m
+//  Git Test
+//
+//  Created by Edward on 21/07/2014.
+//  Copyright (c) 2014 Jin Edward Kim. All rights reserved.
+//
+
+#import "JIN_Test.h"
+
+@implementation JIN_Test
+
+@end
